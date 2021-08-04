8627087d1953dae9d45742aa5e6e2ffd33ab99a7
Felix <felix.flores.andrade@gmail.com>
2021-08-03 22:11:17 -0400

    new module 7 - Rest example

---
+++ 7-Rest/controllers/usuarios.js
@@ -0,0 +1,50 @@
+const {response, request} = require('express');
+
+
+const usuariosGet = (req = request, res = response) => { 
+
+    const {q,nombre = 'No name',apikey, page = 1, limit} = req.query;
+
+    res.status(200).json({
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+
+    const id = req.params.id;
+
+    res.json({
+        msg: 'put API - controlador',
+        put
+    });
+}
+
+const usuariosPost =  (req, res = response) => {
+
+    const {nombre, edad} = req.body;
+    res.json({
+        msg: 'post API - controlador',
+        nombre,
+        edad
+    });
+}
+
+const usuariosDelete =  (req, res = response) => {
+    res.json({
+        msg: 'delete API - controlador'
+    });
+}
+
+
+module.exports = {
+    usuariosGet,
+    usuariosDelete,
+    usuariosPost,
+    usuariosPut
+}
